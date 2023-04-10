7ea3e3df0278f8d2b0413b1a970409b4d489d4b9
Daniel <grezzi@hotmail.com>
2023-04-11 00:14:56 +0100

    add email in db

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,41 +1,55 @@
-import React, { createContext, useState } from 'react';
+import React, {createContext, useState} from 'react';
 
 // importação das funções
-import { fVerifyUser, fHandleSignIn, fHandleSignUp, fHandleSignOut, fDbSetData, fDbGetData, fUserStorage, fGetUserStorage, fCheck } from '../functions';
+import {
+  fVerifyUser,
+  fHandleSignIn,
+  fHandleSignUp,
+  fHandleSignOut,
+  fDbSetData,
+  fDbGetData,
+  fUserStorage,
+  fGetUserStorage,
+  fCheck,
+} from '../functions';
 
 export const AuthContext = createContext({});
 
-export default function AuthProvider({ children }) {
-    const [loading, setLoading] = useState(true);
-    const [dataContext, setDataContext] = useState({});
+export default function AuthProvider({children}) {
+  const [loading, setLoading] = useState(true);
+  const [dataContext, setDataContext] = useState({});
 
-    // Declaração das funções
-    const verifyUser = () => fVerifyUser(setDataContext, setLoading);
-    const handleSignIn = (email, pwd) => fHandleSignIn(userStorage, setDataContext, setLoading, email, pwd);
-    const handleSignUp = (name, email, pwd) => fHandleSignUp(setLoading, dbSetData, handleSignIn, name, email, pwd);
-    const handleSignOut = () => fHandleSignOut(setDataContext, setLoading);
-    const dbSetData = (uid, name, pwd) => fDbSetData(uid, name, pwd);
-    const dbGetData = () => fDbGetData(dataContext);
-    const userStorage = (data) => fUserStorage(data);
-    const getUserStorage = () => fGetUserStorage();
-    // comandos para teste
-    const check = (data) => fCheck(setLoading, loading, data);
+  // Declaração das funções
+  const verifyUser = () => fVerifyUser(setDataContext, setLoading);
+  const handleSignIn = (email, pwd) =>
+    fHandleSignIn(userStorage, setDataContext, setLoading, email, pwd);
+  const handleSignUp = (name, email, pwd) =>
+    fHandleSignUp(setLoading, dbSetData, handleSignIn, name, email, pwd);
+  const handleSignOut = () => fHandleSignOut(setDataContext, setLoading);
+  const dbSetData = (uid, email, name, pwd) =>
+    fDbSetData(uid, email, name, pwd);
+  const dbGetData = () => fDbGetData(dataContext);
+  const userStorage = data => fUserStorage(data);
+  const getUserStorage = () => fGetUserStorage();
+  // comandos para teste
+  const check = data => fCheck(setLoading, loading, data);
 
-    // disponibilizar funções e variaveis para o provider
-    return (
-        <AuthContext.Provider value={{
-            dataContext,
-            loading,
-            setLoading,
-            verifyUser,
-            handleSignIn,
-            handleSignUp,
-            handleSignOut,
-            userStorage,
-            getUserStorage,
-            check,
-        }}>
-            {children}
-        </AuthContext.Provider>
-    )
-};
+  // disponibilizar funções e variaveis para o provider
+  return (
+    <AuthContext.Provider
+      value={{
+        dataContext,
+        loading,
+        setLoading,
+        verifyUser,
+        handleSignIn,
+        handleSignUp,
+        handleSignOut,
+        userStorage,
+        getUserStorage,
+        check,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
